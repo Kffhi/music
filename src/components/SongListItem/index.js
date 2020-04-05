1bc6b7b4d9fcd2312523ed8298ed848cf77bf315
wuhao <asgardusk@gmail.com>
2020-04-05 22:00:58 +0800

    fix: 拼写错误

--- src/components/SongListItem/index.js
+++ src/components/SongListItem/index.js
@@ -16,7 +16,7 @@ const SongListItem = props => {
       <div className={styles.textWrapper}>
         <div className={styles.text}>
           <div className={styles.title}>{listItem.title}</div>
-          <div className={styles.info}><spna className={styles.num}>{listItem.num}</spna>首{listItem.author !== '' ? <span className={styles.author}>by <span>{listItem.author}</span></span> : null}</div>
+          <div className={styles.info}><span className={styles.num}>{listItem.num}</span>首{listItem.author !== '' ? <span className={styles.author}>by <span>{listItem.author}</span></span> : null}</div>
         </div>
         <i className="iconfont icon-more" />
       </div>
