d68afc2f100687b4e0f928495c9d5366a663d55b
wuhao <asgardusk@gmail.com>
2020-04-11 17:33:27 +0800

    feat: 新增prograssBar onClick事件 && dotTouch待完善

--- src/routes/Player/index.js
+++ src/routes/Player/index.js
@@ -1,5 +1,6 @@
 import React, { useState, Fragment, useEffect, useRef } from 'react'
 import className from 'classnames';
+import Toast from '../../components/Toast'
 import { format } from '../../utils/format'
 import styles from './style.less'
 
@@ -8,46 +9,78 @@ const Player = props => {
     history,
     currentIndex = 0
   } = props
+  const audioRef = useRef()
+  const progressBarRef = useRef()
+  const dotRef = useRef()
   const [currentTime, setCurrentTime] = useState('0:00')
   const [showLyric, setShowLyric] = useState(false)
   const [isPlay, setIsPlay] = useState(false)
+  const [offsetWidth, setOffsetWidth] = useState(0)
+  const [touchStart, setTouchStart] = useState(0)
   // const [playSong, setPlaySong] = useState({})
   const playSong = {
     "name": "虹の彼方 (feat. Lasah)",
     "singer": "小瀬村晶 / lasah",
+    "picUrl": "https://i.ytimg.com/vi/KIL_kmtcNcU/maxresdefault.jpg",
     "url": "https://www.kffhi.com/public/images/audio/test.mp3",
-    "time": "07:26"
+    "time": "446"
   }
   const playList = [
     {
       "name": "虹の彼方 (feat. Lasah)",
       "singer": "小瀬村晶 / lasah",
+      "picUrl": "https://i.ytimg.com/vi/KIL_kmtcNcU/maxresdefault.jpg",
       "url": "https://www.kffhi.com/public/images/audio/test.mp3",
-      "time": "07:26"
+      "time": "446"
     },
     {
       "name": "baby I love you",
       "singer": "Shiggy Jr.",
+      "picUrl": "https://i.ytimg.com/vi/suTPx6M8vTQ/hqdefault.jpg",
       "url": "https://www.kffhi.com/public/images/audio/1.mp3",
       "time": "03:44"
     },
     {
       "name": "Enough",
       "singer": "Maisy Kay",
+      "picUrl": "https://i.kfs.io/album/global/7241972,0v1/fit/500x500.jpg",
       "url": "https://www.kffhi.com/public/images/audio/2.mp3",
       "time": "04:34"
     },
     {
       "name": "长恨歌",
       "singer": "五色石南叶",
+      "picUrl": "https://p2.music.126.net/0rDEqHG2be0xmPs8AiAS5A==/2545369418305520.jpg",
       "url": "https://www.kffhi.com/public/images/audio/3.mp3",
       "time": "02:57"
     }
   ]
-  const audioRef = useRef()
+  let touchOffset = 0
 
-  useEffect(() => {
-  }, [])
+  const handleClickProgessBar = e => {
+    setOffsetWidth(e.nativeEvent.offsetX / 10)
+    audioRef.current.currentTime = (e.nativeEvent.offsetX / 10) / 25 * playSong.time
+    audioRef.current.play()
+    setIsPlay(true)
+  }
+
+  const handleDotTouchStart = e => {
+    setTouchStart(e.nativeEvent.touches[0].pageX / 10)
+    console.log('touchStart', touchStart)
+    audioRef.current.pause()
+  }
+
+  const handleDotTouchMove = e => {
+    console.log('touchMove', touchStart)
+    touchOffset = e.nativeEvent.touches[0].pageX / 10 - touchStart
+    setOffsetWidth(touchOffset > 25 ? 25 : touchOffset)
+    audioRef.current.currentTime = touchOffset / 25 * playSong.time
+  }
+
+  const handleDotTouchEnd = () => {
+    audioRef.current.play()
+    setIsPlay(true)
+  }
 
   const handleChangePlayState = () => {
     if (isPlay) {
@@ -59,6 +92,7 @@ const Player = props => {
   }
 
   const handleChangeCurrentTime = () => {
+    setOffsetWidth((audioRef.current.currentTime / playSong.time) * 25)
     setCurrentTime(format(audioRef.current.currentTime))
   }
 
@@ -93,7 +127,7 @@ const Player = props => {
           <div className={className(styles.imgWrapper, {
             [styles.imgWrapperPlaying]: isPlay
           })}>
-            <img src="https://i.ytimg.com/vi/KIL_kmtcNcU/maxresdefault.jpg" alt="" />
+            <img src={playSong.picUrl} alt="" />
           </div>
         }
       </div>
@@ -123,13 +157,13 @@ const Player = props => {
         </div>
         <div className={styles.progressBar}>
           <div className={`${styles.time} ${styles.left}`}>{currentTime}</div>
-          <div className={styles.backLine}>
-            <div className={styles.prograssLine}></div>
-            <div className={styles.dot}>
+          <div className={styles.backLine} ref={progressBarRef} onClick={e => { handleClickProgessBar(e) }}>
+            <div className={styles.prograssLine} style={{ "width": offsetWidth + 'rem' }}></div>
+            <div className={styles.dot} style={{ "left": offsetWidth - 0.3 + 'rem' }} ref={dotRef} onTouchEnd={() => { handleDotTouchEnd() }} onTouchMove={e => { handleDotTouchMove(e) }} onTouchStart={e => { handleDotTouchStart(e) }}>
               <div className={styles.dotColor}></div>
             </div>
           </div>
-          <div className={`${styles.time} ${styles.right}`}>{playSong.time}</div>
+          <div className={`${styles.time} ${styles.right}`}>{format(playSong.time)}</div>
         </div>
         <div className={styles.playCtrlBar}>
           <div className={styles.ctrlIconWrapper}>
@@ -167,7 +201,7 @@ const Player = props => {
   return (
     <div className={styles.player}>
       <div className={styles.backgroundWrapper}>
-        <img src="https://i.ytimg.com/vi/KIL_kmtcNcU/maxresdefault.jpg" alt="" />
+        <img src={playSong.picUrl} alt="" />
       </div>
       <div className={styles.containerWrapper}>
         {renderHeader()}
